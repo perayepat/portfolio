b96264867d9c8f92ece1d37d638e826c4ce447db
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-01-30 17:56:58 +0200

    :star: Contact form button added
    Contact button with modal activation on click

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,43 @@
 const header = document.querySelector("header");
 const sectionOne = document.querySelector(".home");
 const navbar = document.querySelector(".tabs");
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener("click", () =>{
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener("click", () =>{
+        // close button
+        closeopenModal(modal)
+    })
+})
+
+overlay.addEventListener("click", () =>{
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal =>{
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if(modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+
+function closeModal(modal) {
+    if(modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
 
 const sectionOneOptions = {};
 
@@ -23,3 +60,5 @@ const sectionOneObserver = new IntersectionObserver(function(
     sectionOneOptions);
 
     sectionOneObserver.observe(sectionOne);
+
+    
